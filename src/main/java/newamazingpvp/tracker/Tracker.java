c6e7f972b70ccd38a12c40aafea4c11d604307c7
NewAmazingPVP <gamingofpat@gmail.com>
2023-04-19 18:30:50 -0400

    Update Tracker.java

--- src/main/java/newamazingpvp/tracker/Tracker.java
+++ src/main/java/newamazingpvp/tracker/Tracker.java
@@ -20,49 +20,58 @@ public class Tracker extends JavaPlugin implements CommandExecutor {
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        new BukkitRunnable() {
+
+        if (cmd.getName().equalsIgnoreCase("track")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("This command can only be used by a player!");
+                return true;
+            }
+
             Player player = (Player) sender;
+
+            if (args.length == 0) {
+                sender.sendMessage("Usage: /track <player>");
+                return true;
+            }
+
             Player target = Bukkit.getPlayer(args[0]);
-            public void run() {
-                if (cmd.getName().equalsIgnoreCase("track")) {
-                    if (!(sender instanceof Player)) {
-                        sender.sendMessage("This command can only be used by a player!");
-                        return;
-                    }
 
-                    if (args.length == 0) {
-                        sender.sendMessage("Usage: /track <player>");
-                        return;
-                    }
+            if (target == null) {
+                sender.sendMessage("Player not found!");
+                return true;
+            }
 
-
-                    if (target == null) {
-                        sender.sendMessage("Player not found!");
-                        return;
-                    }
-
-                    if (player.getWorld().getEnvironment() == World.Environment.NORMAL && target.getWorld().getEnvironment() == World.Environment.NORMAL) {
-                        player.setCompassTarget(target.getLocation());
-                        player.sendMessage("Compass is now pointing towards " + target.getName() + " in the overworld!");
-                    } else if (player.getWorld().getEnvironment() == World.Environment.NETHER && target.getWorld().getEnvironment() == World.Environment.NETHER) {
-                        ItemStack playersItem = player.getInventory().getItemInMainHand();
-
-                        if (playersItem.getType() == Material.COMPASS) {
-                            CompassMeta compassMeta = (CompassMeta) playersItem.getItemMeta();
+            if (player.getWorld() == target.getWorld()) {
+                player.sendMessage("Compass is now pointing towards " + target.getName());
+                ItemStack playersMainItem = player.getInventory().getItemInMainHand();
+                ItemStack playersOffItem = player.getInventory().getItemInOffHand();
+                if (playersMainItem.getType() == Material.COMPASS) {
+                    new BukkitRunnable() {
+                        public void run() {
+                            CompassMeta compassMeta = (CompassMeta) playersMainItem.getItemMeta();
                             compassMeta.setLodestone(target.getLocation());
                             compassMeta.setLodestoneTracked(true);
-                            playersItem.setItemMeta(compassMeta);
-                            player.sendMessage("Your compass is now tracking " + target.getName() + " in the nether!");
-                        } else {
-                            player.sendMessage("You need to hold a compass to use this command in the nether!");
+                            playersMainItem.setItemMeta(compassMeta);
                         }
-                    } else {
-                        player.sendMessage("The target is not in the same dimension as you!");
-                    }
+                    }.runTaskTimer((Plugin)this, 0L, 0L);
+                } else if (playersOffItem.getType() == Material.COMPASS) {
+                    new BukkitRunnable() {
+                        public void run() {
+                            CompassMeta compassMeta = (CompassMeta) playersOffItem.getItemMeta();
+                            compassMeta.setLodestone(target.getLocation());
+                            compassMeta.setLodestoneTracked(true);
+                            playersOffItem.setItemMeta(compassMeta);
+                        }
+                    }.runTaskTimer((Plugin)this, 0L, 0L);
+                } else {
+                    player.sendMessage("You need to hold a compass to use this command in the nether!");
                 }
+            } else {
+                player.sendMessage("The target is not in the same dimension as you!");
             }
-        }.runTaskTimer((Plugin)this, 0L, 0L);
+            return true;
+        }
 
-        return true;
+        return false;
     }
 }
