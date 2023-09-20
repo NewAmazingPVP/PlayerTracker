f799f938b521db7267e763f934ee6ee91971ee95
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-09-20 14:56:23 +0000

    hi

--- src/main/java/newamazingpvp/tracker/Tracker.java
+++ src/main/java/newamazingpvp/tracker/Tracker.java
@@ -59,64 +59,6 @@ public class Tracker extends JavaPlugin implements CommandExecutor, Listener {
     }
 
 
-    @EventHandler
-    public void playerServerAge(PlayerChatEvent event) {
-        String message = event.getMessage().toLowerCase(); // Convert to lowercase
-        List<String> phrasesToMatch = Arrays.asList(
-                "how old is the server",
-                "when did the server start",
-                "how long has the server been",
-                "what's the server's age",
-                "since when has the server been running",
-                "tell me the server's age",
-                "from when has the server been active",
-                "when was this server created",
-                "how much time has the server been online",
-                "when was the server initiated",
-                "for how long has the server been live",
-                "what is the server's founding date",
-                "how much time has passed since the server's beginning",
-                "when was this server brought into being",
-                "when did the server first become active",
-                "from what time has the server been up",
-                "when did this server open up",
-                "how old is server",
-                "how long has the server been",
-                "how old server",
-                "how old is this server",
-                "when did server start",
-                "server runtime",
-                "what is server age",
-                "server age"
-        );
-
-        for (String phrase : phrasesToMatch) {
-            if (message.contains(phrase)) {
-                LocalDateTime currentTime = LocalDateTime.now();
-                Duration duration = Duration.between(serverStartTime, currentTime);
-                long days = duration.toDays();
-                long hours = duration.toHoursPart();
-                long minutes = duration.toMinutesPart();
-
-                String uptimeMessage = String.format(
-                        "The server started on" + ChatColor.AQUA + " 7/27/23 12:00pm est" + ChatColor.WHITE + " and has been up for" + ChatColor.GOLD + " %d days, %d hours, and %d minutes.",
-                        days, hours, minutes
-                );
-
-                Bukkit.getScheduler().runTaskLater(this, new Runnable() {
-                    @Override
-                    public void run() {
-                        for(Player p : Bukkit.getServer().getOnlinePlayers()) {
-                            p.sendMessage(uptimeMessage);
-                        }
-                    }
-                }, 20);
-                break;
-            }
-        }
-    }
-
-
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("track")) {
             if (!(sender instanceof Player)) {
@@ -192,6 +134,7 @@ public class Tracker extends JavaPlugin implements CommandExecutor, Listener {
 
             trackingPlayers.put(player.getUniqueId(), target.getUniqueId());
             player.sendMessage(ChatColor.GREEN + "Compass is now pointing towards " + target.getName());
+            target.sendMessage(ChatColor.DARK_RED + "" ChatColor.BOLD + "[WARNING] You are being tracked!");
             return true;
         }
 
